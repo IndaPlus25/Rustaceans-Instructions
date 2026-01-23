87ad3deb2117d8cf8b99c3348c6250166e42f09b
Blåpen <violaso@kth.se>
2026-01-23 11:49:34 +0100

    v15: Publish task-15-collections

---
+++ task-15-collections/kattis_template/src/main.rs
@@ -0,0 +1,36 @@
+/***
+ * Template to a Kattis solution.
+ * 
+ * Course litterature.
+ * Course: DD1337 Programming
+ * KTH Royal Institute of Technology
+ * 
+ * Author: Viola Söderlund <violaso@kth.se>
+ * License: MIT
+ * Latest change: 2025-09-04
+ */
+
+use std::io;
+use std::io::prelude::*;
+
+/// Kattis calls main function to run your solution.
+fn main() {
+    // Get standard input stream.
+    let input = io::stdin();
+
+    // Get input lines as strings.
+    // See: https://doc.rust-lang.org/std/io/trait.BufRead.html
+    let mut lines = input
+        .lock()
+        .lines()
+        .map(|_line| _line.ok().unwrap())
+        .collect::<Vec<String>>();
+
+    /* add code here ... */
+
+    // Debug print statement.
+    eprintln!("Kattis print this comment to an irrelevant stream!");
+    
+    // Print result to Kattis.
+    println!("Print to standard output.");
+}
